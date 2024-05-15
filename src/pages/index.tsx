a2f7b14aaccdc827ad5e07316cd2a79b18f139b6
o-ren-zi <s1f102400369@iniad.org>
2024-05-15 22:09:22 +0900

    マインスイーパー

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,30 +3,40 @@ import styles from './index.module.css';
 
 const Home = () => {
   const [samplePos, setSamplepos] = useState(0);
-  const[bombmap,setBombmap]=useState([
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0]
+  const [bombmap, setBombmap] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
 
-  const[userInputs,setUserinputs] = useState([])
-  const board:number[][] = [];
-  for(let y = 0; y < 9; y++)
-    for(let x = 0; x < 9; x++)
-  board.map
-// 再起関数(ゼロ連鎖)
+  const [userInputs, setUserinputs] = useState([]);
+  const board: number[][] = [];
+  for (let y = 0; y < 9; y++) for (let x = 0; x < 9; x++) board.map;
+  // 再起関数(ゼロ連鎖)
 
   console.log(samplePos);
   return (
     <div className={styles.container}>
-      <div className={styles.samplestyle} style={{ backgroundPosition: `${-30 * samplePos}px 0px ` }} />
-      <button onClick={() => setSamplepos((p) => (p + 1) % 14)}>sample</button>
+      <div className={styles.base}>
+        <div className={styles.topboard}>
+          <div className={styles.timeboard} />
+          <div className={styles.emoboard} />
+          <div className={styles.countboard} />
+        </div>
+        <div className={styles.board} />
+
+        {/* <div
+          className={styles.samplestyle}
+          style={{ backgroundPosition: `${-30 * samplePos}px 0px ` }}
+        /> */}
+        {/* <button onClick={() => setSamplepos((p) => (p + 1) % 14)}>sample</button> */}
+      </div>
     </div>
   );
 };
