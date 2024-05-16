fafc5bc2165fccecefa6bb95784842ffe454188d
o-ren-zi <s1f102400369@iniad.org>
2024-05-16 22:51:43 +0900

    マインスイーパー

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,23 +23,25 @@ const Home = () => {
   console.log(samplePos);
   return (
     <div className={styles.container}>
-      <div className={styles.topboard}>
-        <div className={styles.countboard} />
-        <div className={styles.emoboard} />
-        <div className={styles.timeboard} />
-      </div>
       <div className={styles.base}>
-        <div className={styles.bombmap}>
-          {bombmap.map((row, y) =>
-            row.map((color, x) => <div className={styles.cellstyle} key={`${x}-${y}`} />),
-          )}
+        <div className={styles.topboard}>
+          <div className={styles.countboard} />
+          <div className={styles.emoboard} />
+          <div className={styles.timeboard} />
         </div>
+        <div className={styles.board}>
+          <div className={styles.bombmap}>
+            {bombmap.map((row, y) =>
+              row.map((x) => <div className={styles.cellstyle} key={`${x}-${y}`} />),
+            )}
+          </div>
 
-        {/* <div
-          className={styles.samplestyle}
-          style={{ backgroundPosition: `${-30 * samplePos}px 0px ` }}
-        /> */}
-        {/* <button onClick={() => setSamplepos((p) => (p + 1) % 14)}>sample</button> */}
+          {/* <div
+            className={styles.samplestyle}
+            style={{ backgroundPosition: `${-30 * samplePos}px 0px ` }}
+          /> */}
+          {/* <button onClick={() => setSamplepos((p) => (p + 1) % 14)}>sample</button> */}
+        </div>
       </div>
     </div>
   );
